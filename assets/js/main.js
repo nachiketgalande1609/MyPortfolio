ffab88aa2264ebd3745bfa00c38a433504474459
Nachiket Galande <nachiketgalande1609@gmail.com>
2025-04-22 20:21:07 +0530

    Code Fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -474,16 +474,6 @@ document.addEventListener("DOMContentLoaded", function () {
     if (sectionDivider) {
         observer.observe(sectionDivider);
     }
-
-    // Add smooth scroll to all links
-    document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-        anchor.addEventListener("click", function (e) {
-            e.preventDefault();
-            document.querySelector(this.getAttribute("href")).scrollIntoView({
-                behavior: "smooth",
-            });
-        });
-    });
 });
 
 document.addEventListener("DOMContentLoaded", function () {
